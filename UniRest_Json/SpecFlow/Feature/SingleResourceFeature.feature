6d0d2453ae3f8a96cd2435bcc936f17556489bbe
Niluka Gunathilaka <nilukag2002@gmail.com>
2019-08-29 16:51:32 +0530

    Add project files.

---
+++ UniRest_Json/SpecFlow/Feature/SingleResourceFeature.feature
@@ -0,0 +1,9 @@
+﻿Feature: GetSingleResource
+
+
+@mytag
+@mytag
+Scenario: View Resource details
+	Given I have API url for get the resource
+	When I send the request with send parameter
+	Then resource details should display
---
+++ UniRest_Json/SpecFlow/StepDefenition/GetSingleResourceSteps.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace UniRest_Json.SpecFlow.StepDefenition
+{
+    [Binding]
+    public class GetSingleResourceSteps
+    {
+        [Given(@"I have API url for get the resource")]
+        public void GivenIHaveAPIUrlForGetTheResource()
+        {
+            
+        }
+        
+        [When(@"I send the request with send parameter")]
+        public void WhenISendTheRequestWithSendParameter()
+        {
+           
+        }
+        
+        [Then(@"resource details should display")]
+        public void ThenResourceDetailsShouldDisplay()
+        {
+            
+        }
+    }
+}
